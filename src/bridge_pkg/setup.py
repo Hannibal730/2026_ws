9b4d7baf551a927d0e925a0c3525ed258677e8df
Hannibal730 <cds730@naver.com>
2026-07-13 08:49:09 +0900

    feat: add bridge_pkg with vehicle_serial_bridge.py

---
+++ src/bridge_pkg/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'bridge_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'pyserial'],
+    zip_safe=True,
+    maintainer='e2box',
+    maintainer_email='e2b@e2box.co.kr',
+    description='Vehicle serial bridge node for encoder feedback and cmd_vel control.',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'vehicle_serial_bridge = bridge_pkg.vehicle_serial_bridge:main',
+        ],
+    },
+)
